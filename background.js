1304120c586173da7c956885bb13efe3c97b98ce
Matheus Telles <business.mwtelles@gmail.com>
2024-07-16 22:47:52 -0300

    feat: buscando os dados da api do pontomais

--- background.js
+++ background.js
@@ -1,17 +1,16 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log('PontoMais Reminder instalado');
     chrome.alarms.create('checkPonto', { periodInMinutes: 30 });
-  });
-  
-  chrome.alarms.onAlarm.addListener((alarm) => {
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === 'checkPonto') {
-      chrome.tabs.query({ url: 'https://app2.pontomais.com.br/*' }, (tabs) => {
-        if (tabs.length > 0) {
-          chrome.tabs.sendMessage(tabs[0].id, { action: 'checkPonto' });
-        } else {
-          console.error('Nenhuma aba correspondente encontrada.');
-        }
-      });
+        chrome.tabs.query({ url: 'https://app2.pontomais.com.br/*' }, (tabs) => {
+            if (tabs.length > 0) {
+                chrome.tabs.sendMessage(tabs[0].id, { action: 'checkPonto' });
+            } else {
+                console.error('Nenhuma aba correspondente encontrada.');
+            }
+        });
     }
-  });
-  
+});
--- content.js
+++ content.js
@@ -1,31 +1,32 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'checkPonto') {
-      chrome.storage.local.get('pontomais_token', (result) => {
-        if (result.pontomais_token) {
-          const token = result.pontomais_token;
-          fetch('https://api.pontomais.com.br/api/session', {
-            method: 'GET',
-            headers: {
-              'Authorization': `Bearer ${token}`,
-              'Content-Type': 'application/json'
+        chrome.storage.local.get(['pontomais_token', 'client_id', 'uid'], (result) => {
+            if (result.pontomais_token) {
+                fetch('https://api.pontomais.com.br/api/session', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${result.pontomais_token}`,
+                        'Content-Type': 'application/json',
+                        'access-token': result.pontomais_token,
+                        'client': result.client_id,
+                        'uid': result.uid
+                    }
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Dados da sessão:', data);
+                    alert('Verifique seu ponto! Hora de bater o ponto.');
+                    sendResponse({ success: true });
+                })
+                .catch(error => {
+                    console.error('Erro ao obter dados da sessão:', error);
+                    sendResponse({ success: false, error: error.toString() });
+                });
+            } else {
+                console.error('Token não encontrado no armazenamento local');
+                sendResponse({ success: false, error: 'Token não encontrado' });
             }
-          })
-          .then(response => response.json())
-          .then(data => {
-            console.log('Dados da sessão:', data);
-            alert('Verifique seu ponto! Hora de bater o ponto.');
-            sendResponse({ success: true });
-          })
-          .catch(error => {
-            console.error('Erro ao obter dados da sessão:', error);
-            sendResponse({ success: false, error: error.toString() });
-          });
-        } else {
-          console.error('Token não encontrado no armazenamento local');
-          sendResponse({ success: false, error: 'Token não encontrado' });
-        }
-      });
-      return true;
+        });
+        return true;
     }
-  });
-  
+});
